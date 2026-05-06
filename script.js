78662a41df251d9ef97ab7fe60a7b0ef9332b117
Peterson <petersonkaris55@gmail.com>
2026-05-06 23:53:08 +0300

    Implement form submission logic to extract guest data in booking process

--- script.js
+++ script.js
@@ -3,4 +3,21 @@ document.addEventListener('DOMContentLoaded',function (){
     //1.select the elements
     const bookingForm = document.querySelector('#resForm');
     const summaryContainer = document.querySelector('#confirmDetails');
+
+    //2.reservation page logic
+    if(bookingForm) {
+        bookingForm.addEventListener('submit',function (event){
+            event.preventDefault();
+
+            //extract values using a custom data object
+            const guestData = {
+                guestName:document.getElementById('fullName').Value,
+                resDate:document.getElementById('resDate').value,
+                resTime:document.getElementById('resTime').value,
+                partySize:document.getElementById('guests').value,
+                location:document.getElementById('seating').value,
+                createdAt:document.getElementById('')
+            }
+        })
+    }
 })
