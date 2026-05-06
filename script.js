937c7ff9cc3de92d6139651e95e7c702bf32d4da
Peterson <petersonkaris55@gmail.com>
2026-05-06 23:28:13 +0300

    Add event listener to initialize booking form and summary container

---
+++ script.js
@@ -0,0 +1,6 @@
+
+document.addEventListener('DOMContentLoaded',function (){
+    //1.select the elements
+    const bookingForm = document.querySelector('#resForm');
+    const summaryContainer = document.querySelector('#confirmDetails');
+})
